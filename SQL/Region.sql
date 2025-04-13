a7bc3fc3e2a01cf1256f6ed163b311a89dec11f7
Mohamed Elwan <mohamedsherifelwan206@gmail.com>
2025-04-13 19:12:54 +0200

    Add files via upload

---
+++ SQL/Region.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [Region] (
+    [Person]	VARCHAR(512),
+    [Region]	VARCHAR(512)
+);
+
+INSERT INTO [Region] ([Person], [Region]) VALUES
+	('Anna Andreadi', 'West'),
+	('Chuck Magee', 'East'),
+	('Kelly Williams', 'Central'),
+	('Cassandra Brandow', 'South');
